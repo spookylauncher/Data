02035e8e9c3e32627b96623ace015c79c4c0cbfc
Onran0 <kotshel2009@gmail.com>
2024-09-10 18:15:10 +0900

    debug code removed

--- open/script.js
+++ open/script.js
@@ -1,7 +1,3 @@
 window.onload = function() {
-  	var query = 'spookylaunch://open?' + window.location.href.substring(window.location.href.indexOf('#') + 1);
-
-  	window.alert(query);
-
-  	window.location.href = 'spookylaunch://open?' + query;
+  	window.location.href = 'spookylaunch://open?' + window.location.href.substring(window.location.href.indexOf('#') + 1);
 };
